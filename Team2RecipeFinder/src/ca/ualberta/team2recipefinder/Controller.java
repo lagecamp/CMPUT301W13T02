2b00a5d38b3e97a328f5f4d5a470fadb206d18c5
lagecamp <lagecamp@ualberta.ca>
2013-03-03 14:00:32 -0700

    Untracked R.java and added some code for integration

--- Team2RecipeFinder/src/ca/ualberta/team2recipefinder/Controller.java
+++ Team2RecipeFinder/src/ca/ualberta/team2recipefinder/Controller.java
@@ -1,22 +1,35 @@
 package ca.ualberta.team2recipefinder;
 
+import java.util.List;
+
 public class Controller {
-	public void search(String[] keywords, boolean searchLocally, boolean searchFromWeb) {
-		
+	
+	RecipeModel model;
+	IngredientList myKitchen;
+	
+	public Controller(RecipeModel model, IngredientList myKitchen) {
+		this.model = model;
+		this.myKitchen = myKitchen;
+	}
+	
+	public List<Recipe> search(String[] keywords, boolean searchLocally, boolean searchFromWeb) {
+		// for now, it only searches locally
+		return model.searchRecipe(keywords, searchLocally, searchFromWeb);
 	}
 	
 	public void searchWithIngredients(String[] keywords, boolean searchLocally, 
 									  boolean searchFromWeb) {
-		
+		// for now, it only searches locally
+		return model.searchWithIngredients(keyowords, searchLocally, searchFromWeb);
 	}
 	
 	public Recipe getRecipe(int i) {
-		// should return ingredient at index i
+		
 		return new Recipe();
 	}
 	
 	public Recipe[] getRecipes() {
-		// should return the complete recipe list as an array
+		
 		return null;
 	}
 }
