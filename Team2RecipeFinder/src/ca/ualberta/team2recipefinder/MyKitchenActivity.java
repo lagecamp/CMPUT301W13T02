6be78f8edecfd3d487dfa3d3ced7ce8879d9cd16
lagecamp <lagecamp@ualberta.ca>
2013-03-03 13:06:28 -0700

    Added MyKitchen files

---
+++ Team2RecipeFinder/src/ca/ualberta/team2recipefinder/MyKitchenActivity.java
@@ -0,0 +1,12 @@
+package ca.ualberta.team2recipefinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyKitchenActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_kitchen);
+	}
+}
