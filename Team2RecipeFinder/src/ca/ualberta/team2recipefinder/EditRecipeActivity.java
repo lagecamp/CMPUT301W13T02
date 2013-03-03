3ed5b09fc3d5d00146b23666dfe21fc246068176
rhamache <hamacher.ryan@gmail.com>
2013-03-03 14:25:20 -0700

    changes to EditRecipeActivity

--- Team2RecipeFinder/src/ca/ualberta/team2recipefinder/EditRecipeActivity.java
+++ Team2RecipeFinder/src/ca/ualberta/team2recipefinder/EditRecipeActivity.java
@@ -2,18 +2,25 @@ package ca.ualberta.team2recipefinder;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class EditRecipeActivity extends Activity {
 
+	EditText nameEdit;
+	EditText procedureEdit;
+	Recipe currentRecipe = new Recipe();
+	int recipeIndex;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_recipe);
 		
-		int recipeIndex;
-		Recipe currentRecipe = new Recipe();
 		Controller c = RecipeFinderApplication.getController();
 		
 		if (savedInstanceState == null) {
@@ -24,10 +31,28 @@ public class EditRecipeActivity extends Activity {
 			}
 		}
 		
-		EditText nameEdit = (EditText) findViewById(R.id.edit_name);
+		nameEdit = (EditText) findViewById(R.id.edit_name);
 		nameEdit.setText(currentRecipe.getName());
 		
+		procedureEdit = (EditText) findViewById(R.id.edit_procedure);
+		procedureEdit.setText(currentRecipe.getProcedure());
 		
+		Button doneButton = (Button) findViewById(R.id.button_done);
+		doneButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				currentRecipe.setName(nameEdit.getText().toString());
+				currentRecipe.setProcedure(procedureEdit.getText().toString());
+				
+				Controller c = RecipeFinderApplication.getController();
+				if (recipeIndex == -1) {
+					c.addRecipe(currentRecipe);
+				}
+				else {
+					c.replaceRecipe(currentRecipe, recipeIndex);
+				}
+			}		
+		});
 	}
 
 
