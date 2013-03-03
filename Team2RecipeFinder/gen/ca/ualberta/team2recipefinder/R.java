fdd71feffb073305466035b4b07dd26604281a60
lagecamp <lagecamp@ualberta.ca>
2013-03-03 12:54:59 -0700

    Changes to make integration

--- Team2RecipeFinder/gen/ca/ualberta/team2recipefinder/R.java
+++ Team2RecipeFinder/gen/ca/ualberta/team2recipefinder/R.java
@@ -14,33 +14,40 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int btnAdd=0x7f070001;
+        public static final int btnDelete=0x7f070002;
         public static final int btnOk=0x7f07000a;
-        public static final int cbxIngredientsKitchen=0x7f070009;
-        public static final int cbxSearchFromWeb=0x7f070008;
-        public static final int cbxSearchLocally=0x7f070007;
-        public static final int content=0x7f070004;
-        public static final int handle=0x7f070005;
-        public static final int listView1=0x7f070002;
-        public static final int menu_settings=0x7f07000b;
-        public static final int results_label=0x7f070000;
-        public static final int separator=0x7f070001;
-        public static final int slidingDrawer1=0x7f070003;
-        public static final int txtKeywords=0x7f070006;
+        public static final int buttons=0x7f070000;
+        public static final int cbxIngredientsKitchen=0x7f07000d;
+        public static final int cbxSearchFromWeb=0x7f07000c;
+        public static final int cbxSearchLocally=0x7f07000b;
+        public static final int content=0x7f070007;
+        public static final int handle=0x7f070008;
+        public static final int listResults=0x7f070005;
+        public static final int menu_settings=0x7f07000e;
+        public static final int results_label=0x7f070003;
+        public static final int separator=0x7f070004;
+        public static final int slidingDrawer1=0x7f070006;
+        public static final int txtKeywords=0x7f070009;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_search=0x7f030001;
+        public static final int activity_kitchen=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_search=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
         public static final int activity_search=0x7f060001;
     }
     public static final class string {
+        public static final int add=0x7f04000d;
         public static final int app_name=0x7f040000;
+        public static final int delete=0x7f04000e;
         public static final int hello_world=0x7f040001;
         public static final int keywords=0x7f040005;
         public static final int menu_settings=0x7f040002;
         public static final int no_keyword=0x7f04000b;
+        public static final int no_source_search=0x7f04000c;
         public static final int ok=0x7f040009;
         public static final int search=0x7f04000a;
         public static final int search_criteria=0x7f040004;
