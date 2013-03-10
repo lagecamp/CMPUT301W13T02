c28522fd42febf00e6d6bac1c6edb5528ab1007b
lxian <lxian@ualberta.ca>
2013-03-10 15:03:55 -0600

    test ingredient

---
+++ RecipeFinderTests/src/ca/ualberta/team2recipefinder/test/IngredientTest.java
@@ -0,0 +1,15 @@
+package ca.ualberta.team2recipefinder.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+import ca.ualberta.team2recipefinder.Ingredient;
+
+public class IngredientTest{
+	@Test
+	public void testGetType(){
+		Ingredient
+	}
+}
