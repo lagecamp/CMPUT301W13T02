995fe4a07e6f2579f3d28436a3dcbdb9cd1196b7
lxian <lxian@ualberta.ca>
2013-03-28 16:15:33 -0600

    recipe test

--- RecipeFinderTests/src/ca/ualberta/team2recipefinder/test/RecipeTest.java
+++ RecipeFinderTests/src/ca/ualberta/team2recipefinder/test/RecipeTest.java
@@ -179,5 +179,101 @@ public class RecipeTest
         recipe.setOnServer(true);  
         assertEquals("GetOnServer", true , recipe.getOnServer());
 	}
+	
+	
+	@Test
+	public void TestAddPhoto() {
+		Recipe recipe = new Recipe();
+		Bitmap bitmap = new Bitmap();
+		Color color = Color.BLUE;
+		bitmap.setPixel(0, 0, color);
+		recipe.addPhoto(bitmap);
+		assertEquals("Add Photo", color, recipe.getPhoto(0).getPixel(0, 0));
+	}
+	
+	@Test
+	public void TestGetAllPhotos() {
+		Recipe recipe = new Recipe();
+		Bitmap bitmap1 = new Bitmap();
+		Bitmap bitmap2 = new Bitmap();
+		Color color1 = Color.BLUE;
+		Color color2 = Color.GREEN;
+		bitmap1.setPixel(0, 0, color1);
+		bitmap2.setPixel(0, 0, color2);
+		recipe.addPhoto(bitmap1);
+		recipe.addPhoto(bitmap2);
+		List<Bitmap> bitmapList = recipe.getAllPhotos();
+		assertTrue("Get All Photos", (color1 == bitmapList[0].getPixel(0,0) && color2 == bitmapList[1].getPixel(0,0));
+	}
+	
+	@Test
+	public void TestRemovePhoto() {
+		Recipe recipe = new Recipe();
+		Bitmap bitmap = new Bitmap();
+		recipe.addPhoto(bitmap);
+		recipe.removePhoto(0);
+		assertTrue("Remove Photo", recipe.hasPhotos());
+	}
+	
+	@Test
+	public void TestGetPhoto() {
+		Recipe recipe = new Recipe();
+		Bitmap bitmap = new Bitmap();
+		Color color = Color.BLUE;
+		bitmap.setPixel(0, 0, color);
+		recipe.addPhoto(bitmap);
+		assertEquals("Get Photo", color, recipe.getPhoto(0).getPixel(0, 0));
+	}
+	
+	@Test
+	public void TestAddComment() {
+		Recipe recipe = new Recipe();
+		recipe.addComment("testing");
+		assertEquals("Add Comment", "testing", recipe.getComment(0));
+	}
+	
+	@Test
+	public void TestGetAllComments() {
+		Recipe recipe = new Recipe();
+		recipe.addComment("testing1");
+		recipe.addComment("testing2");
+		List<String> comments = recipe.getAllComments();
+		assertTrue("Get All Comments", (comments[0] == "testing1" && comments[1] == "testing2"));
+	}
+
+	@Test
+	public void TestGetComment() {
+		Recipe recipe = new Recipe();
+		recipe.addComment("testing");
+		assertEquals("Get Comment", "testing", recipe.getComment(0));
+	}
+	
+	@Test
+	public void TestSetUserId() {
+		Recipe recipe = new Recipe();
+		recipe.setUserId("testing");
+		assertEquals("Set User Id", "testing", recipe.getUserId());
+	}
+	
+	@Test
+	public void TestGetUserId() {
+		Recipe recipe = new Recipe();
+		recipe.setUserId("testing");
+		assertEquals("Get User Id", "testing", recipe.getUserId());
+	}
+	
+	@Test
+	public void TestSetServerId() {
+		Recipe recipe = new Recipe();
+		recipe.setServerId("testing");
+		assertEquals("Set Server Id", "testing", recipe.getServerId());
+	}
+	
+	@Test
+	public void TestGetServerId() {
+		Recipe recipe = new Recipe();
+		recipe.setServerId("testing");
+		assertEquals("Set Server Id", "testing", recipe.getServerId());
+	}
 
 }
