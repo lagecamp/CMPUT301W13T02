3a82cf5e0a9f362f050060c144abcb44240c1ac4
lagecamp <lagecamp@ualberta.ca>
2013-03-28 14:41:41 -0600

    Added some javadoc comments

--- Team2RecipeFinder/src/ca/ualberta/team2recipefinder/controller/SearchResult.java
+++ Team2RecipeFinder/src/ca/ualberta/team2recipefinder/controller/SearchResult.java
@@ -1,5 +1,11 @@
 package ca.ualberta.team2recipefinder.controller;
 
+/**
+ * This class encapsulates a search result, either from the
+ * server or the client
+ * @author CMPUT 301 Team 2
+ *
+ */
 public class SearchResult {	
 	public final static int SOURCE_LOCAL = 0;
 	public final static int SOURCE_REMOTE = 1;
@@ -9,6 +15,13 @@ public class SearchResult {
 	private long localId;
 	private int source;	
 	
+	/**
+	 * Constructor
+	 * @param name name of the recipe
+	 * @param localId the id in the local database
+	 * @param serverId the id on the server
+	 * @param source the constant the designates the source (server or client)
+	 */
 	public SearchResult(String name,  long localId, String serverId, int source) {
 		this.name = name;
 		this.serverId = serverId;
@@ -16,18 +29,34 @@ public class SearchResult {
 		this.source = source;
 	}
 	
+	/**
+	 * Returns the recipe name
+	 * @return recipe name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Returns the server id
+	 * @return server id
+	 */
 	public String getServerId() {
 		return serverId;
 	}
 
+	/**
+	 * Returns the local id
+	 * @return local id
+	 */
 	public long getLocalId() {
 		return localId;
 	}
 
+	/**
+	 * Returns the source
+	 * @return the integer the identifies the source of the result
+	 */
 	public int getSource() {
 		return source;
 	}	
